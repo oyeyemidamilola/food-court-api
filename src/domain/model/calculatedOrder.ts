080a3261e6db29711331ed34ba39bef09ba72fa6
Damilola Oyeyemi <oyeyemidamilola21@gmail.com>
2024-01-15 11:11:02 +0100

    Feat: Altered calculated order model

--- src/domain/model/calculatedOrder.ts
+++ src/domain/model/calculatedOrder.ts
@@ -9,6 +9,17 @@ export class CalculatedOrder extends Model {
     updated_at: Date;
     deleted_at: Date;
 
+    total_amount: number
+    free_delivery: boolean
+    delivery_fee: number
+    service_charge?: number
+    address_details: { 
+        building_no: string
+        address_line: string
+        name?: string
+        city: string
+    }
+
     static tableName: string = 'calculated_orders'
     static idColumn: string = 'id'
 }
---
+++ src/infrastructure/data/migrations/20240115094717_alterCalculatedOrder.ts
@@ -0,0 +1,20 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    return await knex.schema.alterTable('calculated_orders', builder => {
+        builder.decimal('totalAmount').notNullable().checkNegative()
+        builder.boolean('freeDelivery').notNullable()
+        builder.decimal('deliveryFee').notNullable().checkNegative()
+        builder.decimal('serviceCharge').nullable().checkNegative()
+        builder.json('addressDetails').notNullable()
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    return await knex.schema.alterTable('calculated_orders', builder => {
+        builder.dropColumns('totalAmount','freeDelivery', 'deliveryFee', 'serviceCharge', 'addressDetails')
+    })
+}
+
